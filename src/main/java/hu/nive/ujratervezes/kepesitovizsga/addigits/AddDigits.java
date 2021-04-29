7f1026080af6649d0db528df3b34896aa30594a9
fktester <kofe16@gmail.com>
2021-04-29 10:54:11 +0200

    AddDigits

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -1,4 +1,26 @@
 package hu.nive.ujratervezes.kepesitovizsga.addigits;
 
 public class AddDigits {
+
+    public int addDigits(String input){
+        if (input == null || input.isBlank()){
+            return -1;
+        }
+        char[] temp = input.toCharArray();
+        int result = 0;
+        for (char c: temp) {
+            result += isInteger(c);
+
+        }
+        return result;
+    }
+
+    private int isInteger(char c){
+        try{
+            int a = Integer.parseInt(String.valueOf(c));
+            return a;
+        } catch (NumberFormatException ne){
+            return 0;
+        }
+    }
 }
